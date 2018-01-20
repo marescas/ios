8e94c82ac43a0f0c0f1a3a27f03185a1ed4ba4f8
Marcos Esteve Casademunt <marescasinf@gmail.com>
2018-01-20 18:37:34 +0100

    Convert Km
    A simple App to convert miles to meters

---
+++ MillasKilometros/MillasKilometros/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  MillasKilometros
+//
+//  Created by Marcos Esteve on 20/1/18.
+//  Copyright © 2018 Marcos Esteve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var numberTextField: UITextField!
+    @IBOutlet var selector: UISegmentedControl!
+    @IBOutlet var resultLabel: UILabel!
+    let unidad = 1.609
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resultLabel.text = ""
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func convertClicked(_ sender: UIButton) {
+        if selector.selectedSegmentIndex==0 {
+            let resultado = Double(numberTextField.text!)! * unidad
+            resultLabel.text = String(resultado)
+        }else{
+            let resultado = Double(numberTextField.text!)! / unidad
+            resultLabel.text = String(resultado)
+        }
+    }
+    
+
+}
+
